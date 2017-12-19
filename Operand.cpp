5f43430d6e207023f1ebd56516226d07e069cd96
Florent VIOLIN <fviolin@e1r1p14.42.fr>
2017-12-19 11:43:53 +0100

    work in progress

--- Operand.cpp
+++ Operand.cpp
@@ -1,6 +1,6 @@
 #include "Operand.hpp"
 
-std::string const & Operad::toString() const {
+std::string const & Operand::toString() const {
   std::ostringstream stream;
   std::string *str;
   std::string tmp;
@@ -11,6 +11,31 @@ std::string const & Operad::toString() const {
 
   return *str;
 }
+
+IOperand const *    Operand::createOperand( eOperandType type, std::string const & value ) const {
+  //use array of pointers on member functions with enum values as an array
+}
+
+IOperand const *    operator+( IOperand const & rhs ) {
+
+}
+
+IOperand const *    operator-( IOperand const & rhs ) {
+
+}
+
+IOperand const *    operator*( IOperand const & rhs ) {
+
+}
+
+IOperand const *    operator/( IOperand const & rhs ) {
+
+}
+
+IOperand const *    operator%( IOperand const & rhs ) {
+  
+}
+
 
 std::ostream & operator<<(std::ostream & o, Operand const & obj) {
 //  o << obj.getType() << std::endl
